b3c6c879099d35feab2c99928e67f81fc3c6ef41
Juan Lucas <juanl.bnr@gmail.com>
2023-08-09 15:34:16 -0300

    portifolio atualizado
    foi mudado todo o design do site

--- portifolio.js
+++ portifolio.js
@@ -1,21 +1,37 @@
 
-const topButton = document.querySelector('.topButton');
+const topButton = document.querySelector('#topButton');
 const aboutMeButton = document.querySelector('#aboutMeButton');
 const projectsButton = document.querySelector('#projectsButton');
 const contactButton = document.querySelector('#contactButton');
+const navbar = document.querySelector('#navbar');
 
 topButton.addEventListener("click", function(){
     window.scrollTo(0,0);
 })
 
+document.addEventListener("scroll", ocultar)
+
+function ocultar(){
+    if(window.scrollY < 30){
+        topButton.style.display = 'none';
+    }else{
+        topButton.style.display = 'flex';
+    }
+}
+
+
+
 aboutMeButton.addEventListener("click", function(){
-    window.scrollTo(0,280)
+    window.scrollTo(0,1000)
 })
 
 projectsButton.addEventListener("click" , function(){
-    window.scrollTo(0, 760);
+    window.scrollTo(0, 1878);
 })
 
 contactButton.addEventListener("click", function(){
-    window.scrollTo(0,1200);
+    window.scrollTo(0,2411);
 })
+
+
+ocultar();
